9d3cbf676e6c315b4a790b36b4b6392ed7eac673
theo-commits <me@tlindsey.cloud>
2021-12-15 09:33:13 -0600

    update server name

--- webserver/main.go
+++ webserver/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		group, err := ec2.NewSecurityGroup(ctx, "web-sg", &ec2.SecurityGroupArgs{
+		group, err := ec2.NewSecurityGroup(ctx, "sg", &ec2.SecurityGroupArgs{
 			Description: pulumi.String("Enable HTTP access"),
 			Ingress: ec2.SecurityGroupIngressArray{
 				ec2.SecurityGroupIngressArgs{
@@ -21,7 +21,7 @@ func main() {
 		if err != nil {
 			return err
 		}
-		server, err := ec2.NewInstance(ctx, "web-server", &ec2.InstanceArgs{
+		server, err := ec2.NewInstance(ctx, "server", &ec2.InstanceArgs{
 			Ami:                 pulumi.String("ami-0adc4758ea76442e2"),
 			InstanceType:        pulumi.String("t2.micro"),
 			VpcSecurityGroupIds: pulumi.StringArray{group.Name},

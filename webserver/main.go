1c666b32aef35d6d01a86782811dba10cf4c8532
theo-commits <me@tlindsey.cloud>
2021-12-15 09:32:31 -0600

    S3 bucket and EC2 Instance

---
+++ webserver/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/ec2"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		group, err := ec2.NewSecurityGroup(ctx, "web-sg", &ec2.SecurityGroupArgs{
+			Description: pulumi.String("Enable HTTP access"),
+			Ingress: ec2.SecurityGroupIngressArray{
+				ec2.SecurityGroupIngressArgs{
+					Protocol:   pulumi.String("tcp"),
+					FromPort:   pulumi.Int(80),
+					ToPort:     pulumi.Int(80),
+					CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+		server, err := ec2.NewInstance(ctx, "web-server", &ec2.InstanceArgs{
+			Ami:                 pulumi.String("ami-0adc4758ea76442e2"),
+			InstanceType:        pulumi.String("t2.micro"),
+			VpcSecurityGroupIds: pulumi.StringArray{group.Name},
+		})
+		if err != nil {
+			return err
+		}
+
+		ctx.Export("publicIp", server.PublicIp)
+		ctx.Export("publicHostName", server.PublicDns)
+		return nil
+	})
+}
